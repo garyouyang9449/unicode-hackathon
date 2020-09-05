d1427979a8f050c8fc461615b696a5d1cf77ffd5
Gary Ouyang <zouyang@ucsd.edu>
2020-09-04 18:32:04 -0700

    added html code in register.php

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <section class="container grey-text">
+        <h4 class="center">Register</h4>
+        <form class="white" action="add.php" method="POST">
+            <label>Your Email</label>
+            <input type="text" name="email">
+            <label>Password</label>
+            <input type="text" name="title">
+            <div class="center">
+                <input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+
+</html>
